059550df88d5901f2db30fdeec42584be929dd18
yehongxia <154546410@qq.com>
2021-10-04 20:22:43 +0800

    fix: remove dist

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,7 @@
 npm run build:docs
 
 # 进入生成的构建文件夹
-cd docs/.vuepress/dist
+mv ./docs/.vuepress/dist ./
 
-# 如果你是要部署到自定义域名
-# echo 'www.example.com' > CNAME
+git checkout gh-pages
 
-git init
-git add -A
-git commit -m 'deploy'
-
-# 如果你想要部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:dolly1015/rosy-ui.git gh-pages
-
-cd -
