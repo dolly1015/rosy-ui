bb2a6b5f61616a9b4e5f61a1e6d38bfa17a26fcb
yehongxia <154546410@qq.com>
2021-10-04 20:39:31 +0800

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ npm run build:docs
 
 git checkout gh-pages
 
-rm -rf !(docs/)
+rm -rf *.html
+rm -rf assets/
+rm -rf component/
+rm -rf guide/
+rm -rf favicon.ico
 
-mv ./docs/.vuepress/dist ./
+mv ./docs/.vuepress/dist/* ./
