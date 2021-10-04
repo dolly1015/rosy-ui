26ac320daa5b9834b1a9bab75fb8209485b81abf
yehongxia <154546410@qq.com>
2021-10-04 21:07:50 +0800

    rosy-ui base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ function resolve (dir) {
 
 module.exports = {
     title: 'Rosy UI',
-    base: '/',
+    base: 'https://dolly1015.github.io/rosy-ui/',
     description: 'Rosy UI Document',
     head: [
         ['link', { rel: 'shortcut icon', href: '/favicon.ico' }]
